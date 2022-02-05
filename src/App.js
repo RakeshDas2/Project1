d1ffce8c4add40e59dc53e83210f1a8ff9e38662
RakeshDas2 <rakeshdas612@gmail.com>
2022-02-05 12:45:51 +0530

    added home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import AddEmployee from './components/AddEmployee';
+import Home from './components/Home';
+import Table from './components/Table';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar bg="light" expand="xxl">
+          <Container>
+            <Navbar.Brand>EMS</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/table">Employees</Link>
+                <Link to="/add-employee">Add Employee</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div>
+          <Routes>
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/table" component={Table} />
+            <Route path="/add-employee" component={AddEmployee} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
