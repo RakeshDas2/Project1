359940fd74c2965bf3140a15939f02b1366dc1bb
RakeshDas2 <rakeshdas612@gmail.com>
2022-02-05 15:39:40 +0530

    changes made in navbar

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        <Navbar bg="light" expand="xxl">
+        <Navbar className='navbar navbar-expand-lg navbar-primary bg-dark'>
           <Container>
-            <Navbar.Brand>EMS</Navbar.Brand>
+            <Navbar.Brand style={{color:'white'}}>EMS </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Link to="/table">Employees</Link>
-                <Link to="/add-employee">Add Employee</Link>
+                <Link to="/add-employee" style={{marginLeft:'15px'}}>Add Employee</Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
         <div>
-          <Routes>
+    
             <Route exact={true} path="/" component={Home} />
             <Route path="/table" component={Table} />
             <Route path="/add-employee" component={AddEmployee} />
-          </Routes>
+        
         </div>
       </div>
     </Router>
