bd4156f5f4393b1eb525f22bfdc3ff3fe9d0f1a8
RakeshDas2 <rakeshdas612@gmail.com>
2022-02-05 15:59:52 +0530

    modifing app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import TableShow from './components/TableShow';
 
@@ -8,9 +7,30 @@ import Registration from './components/Registration';
 
 function App() {
   return (
+    <Router>
+      <div className='App'>
+        <Navbar className='navbar navbar-expand-lg navbar-primary bg-dark'>
+          <Container>
+            <Navbar.Brand style={{color:'white'}}>EMS </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/table">Employees</Link>
+                <Link to="/add-employee" style={{marginLeft:'15px'}}>Add Employee</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <div>
+    
+            <Route exact={true} path="/" component={Home} />
+            <Route path="/table" component={Table} />
+            <Route path="/add-employee" component={AddEmployee} />
+        
+        </div>
+      </div>
+    </Router>
 
-
-    </div>
   );
 }
 
